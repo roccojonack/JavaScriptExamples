ba34d56832a5455b4a1a8b41baa0cc6fb6f14c35
Rocco Jonack <rocco.jonack@gmail.com>
2021-04-19 20:58:47 +0000

    playing with summary eval

--- dash_tests/app.py
+++ dash_tests/app.py
@@ -6,10 +6,12 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 import plotly.express as px
 import pandas as pd
 import glob, os
 import csv
+import plotly.graph_objects as go
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -17,13 +19,24 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 10],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+df = []
+names = []
+for f in glob.glob('scenario_1/sum*.csv'):
+    df.append(pd.read_csv(f))
+    names.append(f)
+print(df)
+# df = pd.read_csv('scenario_1/sum.csv')
+# fig = px.line(df, x="name", y="bandwidth")
+fig = go.Figure()
+# Create and style traces
+for i,data in enumerate(df):
+    fig.add_trace(go.Bar(x=df[i]['name'], y=df[i]['bandwidth'], name=names[i]))
+
+fig.update_layout(title='Bandwidth in different scenarios',
+                   xaxis_title='interface name',
+                   yaxis_title='amount of transfers')
+fig.update_layout(barmode='group')
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
@@ -35,28 +48,19 @@ app.layout = html.Div(children=[
     dcc.Graph(
         id='example-graph',
         figure=fig
+    ),
+
+    dash_table.DataTable(
+        id='table',
+        sort_action='native',
+        filter_action='native',
+        columns=[{"name": i, "id": i} for i in df[0].columns],
+        data=df[0].to_dict('records'),
     )
+
 ])
 
 if __name__ == '__main__':
-    data = {}
-    key = {}
-    os.chdir("./")
-    for file in glob.glob("*.csv"):
-        if file=="sum.csv":
-            data[file] = {}
-            key[file]  = []
-            with open(file, newline='') as csvfile:
-                myreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-                for index, lines in enumerate(myreader):
-                    for itIndex,item in enumerate(lines):
-                        if index==0:
-                            print ("dbg:",file,key[file],index,itIndex)
-                            data[file][item] = []
-                            key[file].append(item)
-                        else:
-                            print ("dbg:",file,key[file],index,itIndex)
-                            data[file][key[file][itIndex]].append(item)
     app.run_server(host='192.168.178.30',debug=True)
 
     
