307e975bfe944b2efd157d1e1fbdc3b9d4413dd5
Rocco Jonack <rocco.jonack@gmail.com>
2021-04-20 21:24:16 +0000

    playing with historgram and subplots

--- dash_tests/app.py
+++ dash_tests/app.py
@@ -12,44 +12,40 @@ import pandas as pd
 import glob, os
 import csv
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
 
 df = []
 names = []
 for f in glob.glob('scenario_1/sum*.csv'):
     df.append(pd.read_csv(f))
     names.append(f)
-print(df)
-# df = pd.read_csv('scenario_1/sum.csv')
-# fig = px.line(df, x="name", y="bandwidth")
-fig = go.Figure()
+df1 = pd.read_csv('scenario_1/ncaiu0_axi_nb.read.csv')
+fig = make_subplots(rows=3, cols=2)
+
+# fig = go.Figure()
 # Create and style traces
 for i,data in enumerate(df):
-    fig.add_trace(go.Bar(x=df[i]['name'], y=df[i]['bandwidth'], name=names[i]))
+    fig.add_trace(go.Bar(x=df[i]['name'], y=df[i]['bandwidth'], name=names[i]), row=1, col=1)
+for i,data in enumerate(df):
+    fig.add_trace(go.Bar(x=df[i]['name'], y=df[i]['latencyavg'], name=names[i]), row=1, col=2)
+fig.add_trace(go.Histogram(x=df1['Time'], nbinsx=100, y=df1['full latency'], histfunc='avg'), row=3, col=1)
 
 fig.update_layout(title='Bandwidth in different scenarios',
+                  height=600, 
                    xaxis_title='interface name',
                    yaxis_title='amount of transfers')
 fig.update_layout(barmode='group')
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+    html.H1(children='Dashboard for a Ncore3 scenario'),
 
     dcc.Graph(
         id='example-graph',
         figure=fig
     ),
-
     dash_table.DataTable(
         id='table',
         sort_action='native',
@@ -57,7 +53,6 @@ app.layout = html.Div(children=[
         columns=[{"name": i, "id": i} for i in df[0].columns],
         data=df[0].to_dict('records'),
     )
-
 ])
 
 if __name__ == '__main__':
