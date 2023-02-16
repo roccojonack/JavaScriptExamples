60546e453d278af7f8ae2ea01b4ce1c1a75e5687
Rocco Jonack <rocco@Roccos-MacBook-Pro.fritz.box>
2023-02-16 21:50:43 +0100

    add datatable addition

--- dash_tests/app.py
+++ dash_tests/app.py
@@ -6,6 +6,7 @@ import dash
 from dash import dcc
 from dash import html
 from dash import dash_table
+from dash import Input, Output
 import plotly.express as px
 import pandas as pd
 import glob, os
@@ -22,8 +23,21 @@ for f in glob.glob('scenario_1/sum*.csv'):
     df.append(pd.read_csv(f))
     names.append(f)
 df1 = pd.read_csv('scenario_1/ncaiu0_axi_nb.read.csv')
+df2 = df1['full latency'].value_counts(ascending=True)
+counter = 0
+sum = df2.values.sum()
+running_sum = 0
+indexLat = []
+valueLat = []
+for i in df2:
+    running_sum += i
+    print("tmp", counter, df2.index[counter], i, sum-running_sum)
+    valueLat.append(sum-running_sum)
+    indexLat.append(df2.index[counter])
+    counter += 1
+print( df2.index)
 fig = make_subplots(rows=3, cols=2)
-
+list_of_val = range(10)
 # fig = go.Figure()
 # Create and style traces
 for i,data in enumerate(df):
@@ -31,6 +45,7 @@ for i,data in enumerate(df):
 for i,data in enumerate(df):
     fig.add_trace(go.Bar(x=df[i]['name'], y=df[i]['latencyavg'], name=names[i]), row=1, col=2)
 fig.add_trace(go.Histogram(x=df1['Time'], nbinsx=100, y=df1['full latency'], histfunc='avg'), row=3, col=1)
+fig.add_trace(go.Scatter(x=indexLat, y=valueLat, name='test'), row=3, col=2)
 
 fig.update_layout(  title='Bandwidth in different scenarios'
                   , height=600
@@ -44,6 +59,14 @@ app.layout = html.Div(children=[
         id='example-graph'
         , figure=fig
     ),
+    dcc.RangeSlider(
+        min=int(df1['Time'].iloc[0]),
+        max=int(df1['Time'].iloc[-1]),
+        value=[df1['Time'].iloc[0], df1['Time'].iloc[-1]],
+        step=None,
+        id='year-slider'
+    ),  
+    html.Div(id='my-output'),
     dash_table.DataTable(
         id='table',
         sort_action='native',
@@ -52,6 +75,13 @@ app.layout = html.Div(children=[
         data=df[0].to_dict('records'),
     )
 ])
+
+@app.callback(
+    Output(component_id='my-output', component_property='children'),
+    Input(component_id='year-slider', component_property='value')
+)
+def update_output_div(input_value):
+    return f'Output: {input_value[0],input_value[-1]}'
 
 if __name__ == '__main__':
     app.run_server(debug=True)
